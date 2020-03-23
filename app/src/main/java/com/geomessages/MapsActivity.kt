b8661b9ca5636920a2f6c4597962f0f8baa43091
Vitalii Movchan <vitalii.movchan@valtech.com>
2020-03-23 15:47:11 +0200

    2nd commit

--- app/src/main/java/com/geomessages/MapsActivity.kt
+++ app/src/main/java/com/geomessages/MapsActivity.kt
@@ -40,10 +40,10 @@ private const val GEOFENCING_TAG = "geofencing"
 private const val GEOFENCING_RADIUS_METERS = 100f
 
 private val pois = mapOf(
-//
-//    "Round Tower" to LatLng(55.681336, 12.575771)
-//    ,"Copenhagen Main Library" to LatLng(55.680869, 12.573612)
-//    ,"University of Copenhagen Student Centre" to LatLng(55.680000, 12.573233)
+
+    "Round Tower" to LatLng(55.681336, 12.575771)
+    ,"Copenhagen Main Library" to LatLng(55.680869, 12.573612)
+    ,"University of Copenhagen Student Centre" to LatLng(55.680000, 12.573233)
 
 //     "(1)Памятник мячу" to LatLng(50.002243, 36.234659)
 //    ,"(2)ХАТОБ" to LatLng(49.999200, 36.232449)
@@ -57,18 +57,18 @@ private val pois = mapOf(
 //    ,"(10)Захисників України" to LatLng(49.989703, 36.264812)
 //    ,"(11)стоянка" to LatLng(449.989961, 36.266236)
 
-    "Gothersgade 109, 1123 København" to LatLng(55.683863, 12.576117)
-    , "Gothersgade 67-55" to LatLng(55.683146, 12.578949)
-    , "Внутрений город" to LatLng(55.682697, 12.580550)
-    , "Kvindehuset" to LatLng(55.682322, 12.582028)
-    , "Hech Holding Aps" to LatLng(55.681736, 12.583804)
-    , "Peter Nielsens Mindefond" to LatLng(55.680955, 12.584614)
-    , "Lille Kongensgade" to LatLng(55.679640, 12.585011)
-    , "Holmens Kanal 7" to LatLng(55.678200, 12.586438)
-    , "Knippels Bridge, Дания" to LatLng(55.674724, 12.587130)
-    , "Børnehusbroen" to LatLng(55.672522, 12.591174)
-    , "Parkering" to LatLng(55.670253, 12.595466)
-    , "Christmas Møllers Pl. 3" to LatLng(55.668680, 12.596947)
+//    "Gothersgade 109, 1123 København" to LatLng(55.683863, 12.576117)
+//    , "Gothersgade 67-55" to LatLng(55.683146, 12.578949)
+//    , "Внутрений город" to LatLng(55.682697, 12.580550)
+//    , "Kvindehuset" to LatLng(55.682322, 12.582028)
+//    , "Hech Holding Aps" to LatLng(55.681736, 12.583804)
+//    , "Peter Nielsens Mindefond" to LatLng(55.680955, 12.584614)
+//    , "Lille Kongensgade" to LatLng(55.679640, 12.585011)
+//    , "Holmens Kanal 7" to LatLng(55.678200, 12.586438)
+//    , "Knippels Bridge, Дания" to LatLng(55.674724, 12.587130)
+//    , "Børnehusbroen" to LatLng(55.672522, 12.591174)
+//    , "Parkering" to LatLng(55.670253, 12.595466)
+//    , "Christmas Møllers Pl. 3" to LatLng(55.668680, 12.596947)
 )
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -108,7 +108,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private fun onLocationPermissionGrantedForGeofence() {
         // Geofencing
-        val geofenceClient = LocationServices.getGeofencingClient(this)
+        val geofenceClient = LocationServices.getGeofencingClient(applicationContext)
         val geofenceList = pois.map {
             Geofence.Builder()
                 .setRequestId(it.key)
@@ -124,7 +124,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             addGeofences(geofenceList)
         }.build()
 
-        geofenceClient.addGeofences(geofenceRequest, broadcastPendingIntent)?.run {
+        geofenceClient.addGeofences(geofenceRequest, servicePendingIntent)?.run {
             addOnSuccessListener {
                 Log.d(GEOFENCING_TAG, "Geofences added")
                 sendNotification(this@MapsActivity, "status", "Geofences added")
@@ -198,7 +198,7 @@ class GeofenceBroadcastReceiver : BroadcastReceiver() {
     }
 }
 
-class GeofenceIntentService(name: String) : IntentService(name) {
+class GeofenceIntentService() : IntentService("name") {
     override fun onHandleIntent(intent: Intent?) {
         onReceiveIntent(this, intent ?: return)
     }
